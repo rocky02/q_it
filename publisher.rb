c6850cc6495fe27bce98a5d0a18e4b04099706ac
Glen <glen.noronha@gmail.com>
2019-04-13 08:44:20 +0530

    Added support for bundler

--- publisher.rb
+++ publisher.rb
@@ -1,6 +1,4 @@
 require 'aws-sdk-sqs'
-require 'byebug'
-require_relative 'load_aws'
 
 class Publisher
   
@@ -31,13 +29,3 @@ class Publisher
   end
 end
 
-
-options = ARGV
-if options[0].nil?
-  puts "You must enter a queue name."
-  return 
-end
-puts "Starting Queue Service..."
-sqs_pub = Publisher.new()    
-queue = sqs_pub.create_std_q(options[0])
-sqs_pub.publish_messages(queue.queue_url, options)
