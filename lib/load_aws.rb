aca755aae582e6014a1f14cc7af362f88e504e8c
nehagujar <coolsnow02@gmail.com>
2019-04-25 11:14:06 +0530

    moved the aws.yml file path into constant

--- lib/load_aws.rb
+++ lib/load_aws.rb
@@ -1,15 +1,17 @@
 require 'yaml'
 
+AWS_PATH = File.join(Dir.pwd, 'config/aws.yml')
+
 def generate_aws_yml_file
   sample_aws = {"aws"=>{"access_key_id"=>"", "secret_access_key"=>"", "region"=>""}}
-  File.open(File.join(Dir.pwd, 'config/aws.yml'), 'w') do |file|
+  File.open(AWS_PATH, 'w') do |file|
     file.puts sample_aws.to_yaml
   end
 end
 
 begin
   if File.exists?(File.join(Dir.pwd, 'config/aws.yml'))
-    AWS = YAML.load(File.read('config/aws.yml'))["aws"]
+    AWS = YAML.load(File.read(AWS_PATH))["aws"]
     if AWS.values.any?(&:empty?)
       puts "Fill in the appropriate values for the aws.yml file"
       exit 1
