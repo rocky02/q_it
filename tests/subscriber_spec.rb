fd1c2a2590d228b85b90c3999e969989f1e363f1
nehagujar <coolsnow02@gmail.com>
2019-04-30 15:19:05 +0530

    removed commented code

--- tests/subscriber_spec.rb
+++ tests/subscriber_spec.rb
@@ -20,14 +20,4 @@ RSpec.describe Subscriber do
       expect(Subscriber.valid_sleep_period?(valid_sleep_period)).to be_truthy
     end
   end
-
-  # context '#read_queue' do
-  #   let (:options) { ["https://sqs.ap-south-1.amazonaws.com/123432145678/test_123", 3] }
-  #   let (:subscriber) { Subscriber.new(options)}
-
-  #   it 'should read from queue' do
-  #     expect(subscriber.read_queue).to be_truthy
-  #   end
-  # end
-
 end
