95f0a397264296fe58502531cc1711528858b31c
nehagujar <coolsnow02@gmail.com>
2019-05-16 10:28:39 +0530

    added colorize for readability of console outputs

--- application_config.rb
+++ application_config.rb
@@ -1,2 +1,3 @@
 require 'aws-sdk-sqs'
+require 'colorize'
 Dir.glob(File.join('lib', '**', '*.rb')).each { |file| require_relative "#{file}" }
--- bin/app.rb
+++ bin/app.rb
@@ -18,7 +18,7 @@ class App
       subscriber = Subscriber.new(options)
       subscriber.start
     else
-      raise QItNoServiceError, "Invalid service name. Try #{SERVICES.join(', ')}." if (service.nil? || !SERVICES.include?(service.downcase))
+      raise QItNoServiceError, "Invalid service name. Try #{SERVICES.join(', ')}.".colorize(:red) if (service.nil? || !SERVICES.include?(service.downcase))
     end  
   end
 end
@@ -26,13 +26,13 @@ end
 # Beginning of execution of App.
 options = ARGV
 service = options[0]
-raise QItArgumentError, "Wrong Arguments. Check documentation on how to run the service." if options.length !=3
+raise QItArgumentError, "Wrong Arguments. Check documentation on how to run the service.".colorize(:red) if options.length !=3
 
 begin
   app = App.new
   app.configure_aws_file
-  puts "Starting Queue Service..."
+  puts "Starting Queue Service...".colorize(:cyan)
   app.run_service(options)
 rescue ArgumentError => e
-  puts "There is an exception in the #{service} service #{e.inspect}"
+  puts "There is an exception in the #{service} service #{e.inspect}".colorize(:red)
 end
