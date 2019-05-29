a3b2f72273da4b225b28179e711e72501261de87
nehagujar <coolsnow02@gmail.com>
2019-05-29 11:03:40 +0530

    changes to aws_loader spec

--- spec/aws_loader_spec.rb
+++ spec/aws_loader_spec.rb
@@ -7,19 +7,20 @@ RSpec.describe AwsLoader do
 
   let (:mod_test) { ModuleTest.new }
 
+  before do
+    stub_const('AwsLoader::AWS_PATH', File.join(Application.root, 'aws.yml'))
+  end
+
   context 'aws.yml file' do    
     context '#configure_aws_file' do
       it 'should call #generate_aws_yml_file' do
-        stub_const('AwsLoader::AWS_PATH', nil)
         allow(File).to receive(:exists?).with(AwsLoader::AWS_PATH).and_return(false)
-        expect(mod_test).to receive(:generate_aws_yml_file)
-        mod_test.configure_aws_file
+        expect{ mod_test.configure_aws_file }.to raise_error(SystemExit)
       end
     end
 
     context 'no aws key values' do
       it 'should log error for no values for the aws config keys in it' do
-        stub_const('AwsLoader::AWS_PATH', File.join(Application.root, 'aws.yml'))
         allow(File).to receive(:exists?).with(AwsLoader::AWS_PATH).and_return(true)
         allow(File).to receive(:read).with(AwsLoader::AWS_PATH).and_return({'aws' => {"access_key_id"=>"", "secret_access_key"=>"", "region"=>""}})
         allow(YAML).to receive(:load).and_return({ 'aws' => { "access_key_id"=>"", "secret_access_key"=>"", "region"=>"" } })
@@ -29,10 +30,8 @@ RSpec.describe AwsLoader do
 
     context 'aws.yml exists' do 
       it 'should not generate aws.yml file' do
-        stub_const('AwsLoader::AWS_PATH', File.join(Application.root, 'aws.yml'))
         allow(File).to receive(:exists?).with(AwsLoader::AWS_PATH).and_return(true)
-        expect(mod_test).to_not receive(:generate_aws_yml_file)
-        mod_test.configure_aws_file
+        expect{ mod_test.configure_aws_file }.to raise_error(SystemExit)
       end
     end
 
@@ -40,7 +39,6 @@ RSpec.describe AwsLoader do
       let (:file) { double('file') }
       
       it 'should generate aws.yml file write the expected keys' do
-        stub_const('AwsLoader::AWS_PATH', File.join(Application.root, 'aws.yml'))
         sample_aws = {"aws"=>{"access_key_id"=>"", "secret_access_key"=>"", "region"=>""}}.to_yaml
         expect(File).to receive(:open).with(AwsLoader::AWS_PATH, 'w').and_yield(file)
         expect(file).to receive(:puts).with(sample_aws)
