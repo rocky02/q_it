30c782d81bb648e98cea422bdd65d37c5c73ebf0
nehagujar <coolsnow02@gmail.com>
2019-05-29 10:23:57 +0530

    minor modifications to test for 100% coverage

--- spec/aws_loader_spec.rb
+++ spec/aws_loader_spec.rb
@@ -17,16 +17,15 @@ RSpec.describe AwsLoader do
       end
     end
 
-    # context 'no aws key values' do
-    #   it 'should log error for no values for the aws config keys in it' do
-    #     stub_const('AwsLoader::AWS_PATH', File.join(Application.root, 'aws.yml'))
-    #     allow(File).to receive(:exists?).with(AwsLoader::AWS_PATH).and_return(true)
-    #     allow(File).to receive(:read).with(AwsLoader::AWS_PATH).and_return({'aws' => {"access_key_id"=>"", "secret_access_key"=>"", "region"=>""}})
-    #     allow(YAML).to receive(:load).and_return({ 'aws' => { "access_key_id"=>"", "secret_access_key"=>"", "region"=>"" } })
-    #     expect(Application).to receive_message_chain(:log, :error).with("Fill in the appropriate values for the aws.yml file")
-    #     mod_test.configure_aws_file
-    #   end
-    # end
+    context 'no aws key values' do
+      it 'should log error for no values for the aws config keys in it' do
+        stub_const('AwsLoader::AWS_PATH', File.join(Application.root, 'aws.yml'))
+        allow(File).to receive(:exists?).with(AwsLoader::AWS_PATH).and_return(true)
+        allow(File).to receive(:read).with(AwsLoader::AWS_PATH).and_return({'aws' => {"access_key_id"=>"", "secret_access_key"=>"", "region"=>""}})
+        allow(YAML).to receive(:load).and_return({ 'aws' => { "access_key_id"=>"", "secret_access_key"=>"", "region"=>"" } })
+        expect{ mod_test.configure_aws_file }.to raise_error(SystemExit)
+      end
+    end
 
     context 'aws.yml exists' do 
       it 'should not generate aws.yml file' do
